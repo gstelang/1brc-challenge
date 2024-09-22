280ab4aa20fdce2b9d33c56ffad042d8c63ca847
Gaurang Telang <gstelang@gmail.com>
2024-09-22 12:38:49 -0700

    get sync.map back

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ type temperature struct {
 	num           int
 }
 
-type temperatureReading struct {
-	station []byte
-	value   float64
-}
-
 // Small enough to avoid exhausting memory, but
 // large enough to minimize overhead from creating and managing goroutines.
 // 1 million rows
@@ -27,35 +22,7 @@ const chunkSize = 4096 * 1000 // 4 MB chunks
 const chanSize = 1000         // 1000 * 1 million = 1 billion
 
 var dataChan = make(chan [][]byte, chanSize)
-var readingsChan = make(chan temperatureReading, batchSize)
-
-var resultMap = make(map[string]temperature)
-
-func aggregateTemperatures(aggWg *sync.WaitGroup) {
-	defer aggWg.Done()
-
-	for reading := range readingsChan {
-		currentVal := reading.value
-		currentStation := string(reading.station)
-		temp, exists := resultMap[currentStation]
-		if !exists {
-			resultMap[currentStation] = temperature{
-				min: currentVal,
-				max: currentVal,
-				sum: currentVal,
-				num: 1,
-			}
-		} else {
-			resultMap[currentStation] = temperature{
-				min: min(temp.min, currentVal),
-				max: max(temp.max, currentVal),
-				sum: temp.sum + currentVal,
-				num: temp.num + 1,
-			}
-		}
-	}
-
-}
+var resultMap sync.Map
 
 func processLine(line []byte) {
 	if len(line) == 0 {
@@ -65,14 +32,31 @@ func processLine(line []byte) {
 	if splitIndex == -1 {
 		return
 	}
-	station := line[:splitIndex]
+	station := string(line[:splitIndex])
 	reading, err := strconv.ParseFloat(string(line[splitIndex+1:]), 64)
 	if err != nil {
 		return
 	}
-	readingsChan <- temperatureReading{
-		station: station,
-		value:   reading,
+	// Load the current value from the map
+	if val, ok := resultMap.Load(station); ok {
+		// If the station exists, update its temperature record
+		temp := val.(temperature)
+		updatedTemp := temperature{
+			max: max(temp.max, reading),
+			min: min(temp.min, reading),
+			num: temp.num + 1,
+			sum: temp.sum + reading,
+		}
+		resultMap.Store(station, updatedTemp)
+	} else {
+		// If the station does not exist, create a new temperature record
+		newTemp := temperature{
+			max: reading,
+			min: reading,
+			num: 1,
+			sum: reading,
+		}
+		resultMap.Store(station, newTemp)
 	}
 }
 
@@ -111,10 +95,6 @@ func setTemperatureReading(fileLocation string) error {
 	batch := make([][]byte, 0, batchSize)
 	buffer := make([]byte, chunkSize)
 	lastLineRead := make([]byte, 0)
-
-	var aggWg sync.WaitGroup
-	aggWg.Add(1)
-	go aggregateTemperatures(&aggWg)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -164,31 +144,24 @@ func setTemperatureReading(fileLocation string) error {
 
 	close(dataChan)
 	wg.Wait()
-	close(readingsChan)
-	aggWg.Wait()
 	return nil
 }
 
 func formatOutput() string {
 	out := "{"
-	total := len(resultMap)
-	count := 0
-	for key, value := range resultMap {
-		// min, mean, max in this order
-		formattedMin := fmt.Sprintf("%.1f", value.min)
-		formattedMean := fmt.Sprintf("%.1f", value.sum/float64(value.num))
-		formattedMax := fmt.Sprintf("%.1f", value.max)
 
-		out += key + "=" + formattedMin + "/" + formattedMean + "/" + formattedMax
-		count++
-
-		if count != total {
-			out += ", "
-		} else {
-			out += "}"
-		}
-	}
-	return out
+	// sync.Map does not have len... so use Range
+	resultMap.Range(func(key, value interface{}) bool {
+		k := key.(string)
+		v := value.(temperature)
+		// Format min, mean, and max values
+		formattedMin := fmt.Sprintf("%.1f", float64(v.min))
+		formattedMean := fmt.Sprintf("%.1f", float64(v.sum)/float64(v.num))
+		formattedMax := fmt.Sprintf("%.1f", float64(v.max))
+		out += k + "=" + formattedMin + "/" + formattedMean + "/" + formattedMax + ", "
+		return true
+	})
+	return out[:len(out)-2] + "}"
 }
 
 func main() {
@@ -197,6 +170,5 @@ func main() {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	output := formatOutput()
-	writeToFile(output)
+	writeToFile(formatOutput())
 }
